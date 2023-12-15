a2e440deb1a990f4aca439e51add9cb48c7c12b8
Rahul Khullar <khullarrahul09@gmail.com>
2023-12-15 21:57:25 +0530

    Chapter01-Inception

--- App.js
+++
@@ -1,4 +0,0 @@
-const heading = React.createElement("h1", {id:"heading",xyz:"abc"}, "Hello World from React!");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
